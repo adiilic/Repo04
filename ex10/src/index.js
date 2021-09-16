ddb4449daceb8526a6db347f0a74e47830bfdbbf
adiilic <adiilic12@gmail.com>
2021-09-16 16:43:23 +0200

    day 04

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+
+    // only change code below this line
+ var myFirstName ="Adi";
+ var myLastName= "Ilic";
+ var myHomeTown ="Sarajevo";   
+    // only change code above this line
+
+
+    return {
+    myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
