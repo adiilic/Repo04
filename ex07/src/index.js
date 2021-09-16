996c0a8139d31e3f1e91f59f585fd7c9cae59681
adiilic <adiilic12@gmail.com>
2021-09-16 23:50:06 +0200

    push

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -8,7 +8,7 @@ function main(numOne, numTwo) {
     return remainder;
 }
 
-console.log(main(5, 2)); // Change this line
-console.log(main(15, 4)); // Change this line
-console.log(main(102, 25)); // Change this line
+console.log(main(5, 2)); 
+console.log(main(15, 4)); 
+console.log(main(102, 25)); 
 module.exports = main;
