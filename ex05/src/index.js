f8dde5afafd55545bac20a7dd7cfb6ca0010949c
adiilic <adiilic12@gmail.com>
2021-09-16 23:48:04 +0200

    correct

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -10,7 +10,7 @@ function main(numOne, numTwo) {
     return product;
 }
 
-console.log(main(2.5, 2.0)); // Change this line
-console.log(main(5.5, 5.5)); // Change this line
-console.log(main(100.25, 43.20)); // Change this line
+console.log(main(2.5, 2.0)); 
+console.log(main(5.5, 5.5)); 
+console.log(main(100.25, 43.20)); 
 module.exports = main;
